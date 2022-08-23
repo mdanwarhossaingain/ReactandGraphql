b98e10181b6251c35a90d99e2c1cd458dc8f5582
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-08-23 18:11:47 +0600

    api done

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,6 +1,55 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {GraphQLClient,gql} from 'graphql-request'
+
+const gaphcms =new GraphQLClient(
+   "https://api-ap-south-1.hygraph.com/v2/cl75u6z1j5kcu01ujb6go8o02/master"
+)
+
+const QUERY = gql`
+  {
+    posts{
+      id,
+      title,
+      datePublished,
+      slug,
+      content{
+        html
+      }
+      author{
+        name,
+        avatar{
+          url
+        }
+      }
+
+      coverPhoto{
+        publishedAt{
+          createdBy{
+            id
+          }
+          url
+        }
+      }
+
+
+    }
+  }
+
+`
+
+export async function getStaticProps(){
+  const {posts} =await gaphcms.request(QUERY);
+  return{
+    props:{
+      posts,
+    },
+
+    revalidate:10,
+  }
+}
+
+
 
 export default function Home() {
   return (
@@ -12,58 +61,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+       
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   )
 }
